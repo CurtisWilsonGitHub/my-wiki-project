e4934974913863595ad06330b3d45ffa20386651
Curtis Wilson <curtiswemail@gmail.com>
2018-04-06 22:10:58 -0600

    start over

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_for :users
+  get 'welcome/index'
+
+  get 'welcome/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
